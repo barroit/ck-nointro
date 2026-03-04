e5dad9ee60d294878032469fd2d9d4e875b5ac52
Jiamu Sun <barroit@linux.com>
2026-03-04 11:47:18 +0900

    init: add entry point
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ core-script/init.cs
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright 2026 Jiamu Sun <barroit@linux.com>
+ */
+
+using UnityEngine;
+
+using PugMod;
+
+public class init : IMod {
+
+public void EarlyInit()
+{
+}
+
+public void Init()
+{
+}
+
+public void ModObjectLoaded(Object _) {}
+
+public void Update() {}
+
+public void Shutdown() {}
+
+} /* class mikufish */
---
+++ core-script/patch.m4
@@ -0,0 +1,7 @@
+dnl SPDX-License-Identifier: GPL-3.0-or-later
+dnl
+divert(-1)
+
+changequote([[, ]])
+
+divert(0)dnl
